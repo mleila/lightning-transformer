61001c0164aee9aeb45b6320bab996c93e286567
Mohamed Leila <mohamed.laila@mail.mcgill.ca>
2021-02-03 14:24:00 -0500

    add new package

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
+    name='transformer',
     version='0.0.0',
     description='Describe Your Cool Project',
-    author='',
-    author_email='',
+    author='Mohamed Leila',
+    author_email='mohamed.leila.1989@gmail.com',
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',
+    url='https://github.com/mleila/lightning-transformer',
     install_requires=['pytorch-lightning'],
     packages=find_packages(),
 )
